b5da58c33afdab472bbb3c2da761cff9ca74c0e4
bhabanaagrawal1 <bhabanaagrawal1111@gmail.com>
2025-04-02 21:04:10 +0530

    did slight changes

--- bhavna/week_1_ITP/QUESTION_7.cpp
+++ bhavna/week_1_ITP/QUESTION_7.cpp
@@ -4,7 +4,7 @@ int main(){
     int n,a=1;
     cout<<"Enter the number of rows: ";
     cin>>n;
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<=3;i++){
         for(int j=1;j<=n;j++){
             if(j<n){
                cout<<a<<"*";
